f527c69a8bf49170f537ee874fa67590c3b12899
Andrii Maliuta <quadr988@gmail.com>
2020-10-02 16:15:00 +0300

    refactored

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -4,15 +4,20 @@ import PokemonsDataService from '../api/PokemonsDataService';
 class Pokemon extends Component {
   constructor(props) {
     super(props);
-    this.state = { pokemon: '' };
+    this.state = { id: '', name: '', image: '', weight: 0 };
   }
 
   componentDidMount() {
     console.log('Inside Pokemon page');
     PokemonsDataService.getPokemonByName(this.props.match.params.name)
       .then((response) => {
-        console.log(response.data);
-        this.setState((state) => ({ pokemon: response.data }));
+        // console.log(response.data.sprites.other.dream_world.front_default);
+        this.setState((state) => ({
+          id: response.data.id,
+          name: response.data.name,
+          image: response.data.sprites.other.dream_world.front_default,
+          weight: response.data.weight,
+        }));
         console.log(this.state.pokemon);
       })
       .catch((error) => console.log(error));
@@ -21,8 +26,9 @@ class Pokemon extends Component {
   render() {
     return (
       <div className='pokemon-root'>
-        <h4>Pokemon {this.state.pokemon.name}</h4>
-        <p>Weight: {this.state.pokemon.weight}</p>
+        <h4>Pokemon {this.state.name}</h4>
+        <p>Weight: {this.state.weight}</p>
+        {this.state.image && <img src={this.state.image} alt='pokemon' />}
       </div>
     );
   }
